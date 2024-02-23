4f83673ee51a80679620985896449f941793b766
Tobias Baunbaek <freeall@gmail.com>
2024-02-23 14:27:32 +0100

    Write to file

--- index.js
+++ index.js
@@ -1,67 +1,14 @@
 import DHT from 'hyperdht'
-import axios from 'axios'
-import { flatten } from 'flat'
 import fs from 'fs'
 
 const node = new DHT()
 const server = node.createServer()
-
-// sendDataToLoki({
-//   "caller": {
-//     "functionName": "_observeSignalEvents",
-//     "props": {
-//       "swarmId": "4mhsoa8riyxu7ozwnsfrbhyj3nqxbbb94h4bgsa84y46ybh4a11y",
-//       "signal": {
-//         "description": {
-//           "type": "answer",
-//           "sdp": "v=0\r\no=- 507648..."
-//         }
-//       }
-//     }
-//   },
-//   "id": "null",
-//   "object": {
-//     "id": "1",
-//     "className": "KeetCall",
-//     "props": {
-//       "swarmId": "4mhsoa8riyxu7ozwnsfrbhyj3nqxbbb94h4bgsa84y46ybh4a11y",
-//       "signal": {
-//         "description": {
-//           "type": "answer",
-//           "sdp": "v=0\r\no=- 507648..."
-//         }
-//       }
-//     }
-//   }
-// })
-
-async function sendDataToLoki (json) {
-  const labels = flatten(json, { delimiter: '_' }) // Turn `{ object: { id: 1 } }` into `{ object_id: 1 }`
-  Object.keys(labels).forEach(key => { labels[key] = '' + labels[key] }) // All values have to be strings for loki
-  delete labels.caller_props_signal_description_sdp
-  delete labels.object_props_signal_description_sdp
-
-  return axios.post('http://localhost:3100/loki/api/v1/push', {
-    streams: [
-      {
-        stream: {
-          job: 'keet',
-          ...labels
-        },
-        values: [
-          [
-            `${Date.now()}000000`,
-            JSON.stringify(json)
-          ]
-        ]
-      }
-    ]
-  })
-}
+let logFilename = 'tmp.log'
 
 server.on('connection', socket => {
   socket.on('error', err => console.error(err))
   console.log('got connection')
+
   socket.on('data', async data => {
     try {
       socket.pause()
@@ -92,6 +39,50 @@ async function main () {
     secretKey: Buffer.from(kp.secretKey, 'hex')
   }
 
+  logFilename = `${kp.publicKey}.log`
   await server.listen(keyPair)
   console.log(`server started on ${keyPair.publicKey.toString('hex')}`)
 }
+
+async function sendDataToLoki (json) {
+  const logEntry = JSON.stringify(json)
+  const timestamp = new Date().toISOString()
+
+  return fs.promises.appendFile(logFilename, `${timestamp} ${logEntry}\n`)
+
+  // Leaving this here, in case we might need the labels part again
+  // const labels = flatten(json, { delimiter: '_' }) // Turn `{ object: { id: 1 } }` into `{ object_id: 1 }`
+  // Object.keys(labels).forEach(key => { labels[key] = '' + labels[key] }) // All values have to be strings for loki
+  // delete labels.caller_props_signal_description_sdp
+  // delete labels.object_props_signal_description_sdp
+}
+
+// sendDataToLoki({
+//   "caller": {
+//     "functionName": "_observeSignalEvents",
+//     "props": {
+//       "swarmId": "4mhsoa8riyxu7ozwnsfrbhyj3nqxbbb94h4bgsa84y46ybh4a11y",
+//       "signal": {
+//         "description": {
+//           "type": "answer",
+//           "sdp": "v=0\r\no=- 507648..."
+//         }
+//       }
+//     }
+//   },
+//   "id": "null",
+//   "object": {
+//     "id": "1",
+//     "className": "KeetCall",
+//     "props": {
+//       "swarmId": "4mhsoa8riyxu7ozwnsfrbhyj3nqxbbb94h4bgsa84y46ybh4a11y",
+//       "signal": {
+//         "description": {
+//           "type": "answer",
+//           "sdp": "v=0\r\no=- 507648..."
+//         }
+//       }
+//     }
+//   }
+// })
+
